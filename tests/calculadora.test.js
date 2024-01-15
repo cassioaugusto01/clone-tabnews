8d8e782f11abe2c059eedcc0f993ec7787cfa930
Cássio Augusto Rosa Machado <cassio_0101@hotmail.com>
2024-01-15 12:42:34 -0300

    Testes unitarios

--- tests/calculadora.test.js
+++ tests/calculadora.test.js
@@ -1,3 +1,16 @@
-test("espero que 1 seja 1", () => {
-  expect(1).toBe(1);
-})
+const calculadora = require("../models/calculadora.js");
+
+test("somar 2 + 2 deveria retornar 4", () =>{
+  const resultado = calculadora.somar(2,2);
+  expect(resultado).toBe(4);
+});
+
+test("somar 5 + 100 deveria retornar 105", () =>{
+  const resultado = calculadora.somar(5,100);
+  expect(resultado).toBe(105);
+});
+
+test("somar 'banana' + 100 deveria retornar 'Erro'", () =>{
+  const resultado = calculadora.somar('banana',100);
+  expect(resultado).toBe("Erro");
+});
