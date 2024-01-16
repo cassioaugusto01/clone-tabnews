5c63a1aead8c542161a0d4cc7218d3ad80eb9338
cassioaugusto01 <37603843+cassioaugusto01@users.noreply.github.com>
2024-01-16 23:07:13 +0000

    add get.test.js file

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,7 @@
 test("GET TO /api/v1/status SHOULD RETURN 200 OK", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status")
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  console.log(responseBody);
 });
