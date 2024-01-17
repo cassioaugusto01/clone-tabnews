d07c41c37cb454f6b9150df2288594f9251545cb
cassioaugusto01 <37603843+cassioaugusto01@users.noreply.github.com>
2024-01-17 16:11:28 +0000

    Tratamento de excessão no database.js

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -15,6 +15,7 @@ async function status(request, response) {
   const databaseName = process.env.POSTGRES_DB;
   const databaseOpenedConnectionsResult = await database.query({
     text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName]
   });
     const databaseOpenedConnectionsValue =
       databaseOpenedConnectionsResult.rows[0].count;
